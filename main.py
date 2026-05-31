cbf6fef1f6b30bef548bfba2a93ef1a3c1a12909
AnthonyLHall <anthonylouishall2003@gmail.com>
2026-05-30 21:37:38 -0400

    Created Processed Directory, Created File Types

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ import shutil
 
 # Processed File Directory
 DIRECTORY = r"C:\Users\antde\OneDrive\Desktop\Intelligent-Document-Routing"
+processed = "Processed"
+
+types = ["invoice", "resume", "contract"]
 
 for file in os.listdir(DIRECTORY):
 
+    print("Current File: " + file)
     # join file paths
     file_dir = os.path.join(DIRECTORY, file)
     read_file = open(file_dir, "r")
 
+    # read file
     contents = read_file.read()
-    print(contents)
+
+    for file_type in types:
+        if file_type.lower() in contents.lower():
+            processed_dir = os.path.join(DIRECTORY, processed)
+            if not os.path.exists(processed_dir):
+                os.makedirs(processed_dir)
+            
+
+            
