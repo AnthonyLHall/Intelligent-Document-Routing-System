6cebc81d732ec4f205fefb45b5bd01c7173331bb
AnthonyLHall <anthonylouishall2003@gmail.com>
2026-05-30 21:09:14 -0400

    read files added

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os
 import shutil
-import pymupdf
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import Pipeline
 
+# Processed File Directory
+DIRECTORY = r"C:\Users\antde\OneDrive\Desktop\Intelligent-Document-Routing"
 
+for file in os.listdir(DIRECTORY):
+
+    # join file paths
+    file_dir = os.path.join(DIRECTORY, file)
+    read_file = open(file_dir, "r")
+
+    contents = read_file.read()
+    print(contents)
