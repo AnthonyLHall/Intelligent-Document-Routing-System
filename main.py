1e06128f6b57f66fad71fa415a0231a6256de625
AnthonyLHall <anthonylouishall2003@gmail.com>
2026-05-30 21:51:32 -0400

    error handling for reading files skips processed folder

--- main.py
+++ main.py
@@ -8,20 +8,27 @@ processed = "Processed"
 types = ["invoice", "resume", "contract"]
 
 for file in os.listdir(DIRECTORY):
+    if file != processed:
+        print("Current File: " + file)
+        # join file paths
+        file_dir = os.path.join(DIRECTORY, file)
 
-    print("Current File: " + file)
-    # join file paths
-    file_dir = os.path.join(DIRECTORY, file)
-    read_file = open(file_dir, "r")
+        try:
+            read_file = open(file_dir, "r")
+        except:
+            print("File/Folder is not Obtainable: " + file)
 
-    # read file
-    contents = read_file.read()
+        # read file
+        contents = read_file.read()
+        
+        for file_type in types:
+            if file_type.lower() in contents.lower():
+                processed_dir = os.path.join(DIRECTORY, processed)
+                if not os.path.exists(processed_dir):
+                    os.makedirs(processed_dir)
 
-    for file_type in types:
-        if file_type.lower() in contents.lower():
-            processed_dir = os.path.join(DIRECTORY, processed)
-            if not os.path.exists(processed_dir):
-                os.makedirs(processed_dir)
-            
+                
+                
+                print(file_type)
 
             
