e44ef259d28f071ea379a01b475a2a9c1f7c46ab
AnthonyLHall <anthonylouishall2003@gmail.com>
2026-06-01 01:47:47 -0400

    fixed get invoice data

--- main.py
+++ main.py
@@ -7,11 +7,37 @@ processed = "Processed"
 
 types = ["invoice", "resume", "contract"]
 
-def get_doc_type(content):
+invoice_field = {}
+resume_field = {}
+contract_field = {}
+
+def get_invoice_data(content):
+
+    global vendor
+    global doc_type
+
+    vendor = None
+    doc_type = None
+
     for line in content.splitlines():
-            if "Document Type: " in line:
-                doc_type = line.split("Document Type:")
-                return doc_type[1]
+        if "Document Type:".lower() in line.lower():
+            doc_type_split = line.split("Document Type:")
+            doc_type = doc_type_split[1]
+            
+        if "Vendor:".lower() in line.lower():
+            vendor_split = line.split("Vendor:")
+            vendor = vendor_split[1]
+
+        if "invoice number:".lower() in line.lower():
+            invoice_num_split = line.split("Invoice Number:")
+            invoice_num = invoice_num_split[1]
+            
+    return doc_type, vendor, invoice_num
+            
+
+processed_dir = os.path.join(DIRECTORY, processed)
+if not os.path.exists(processed_dir):
+    os.makedirs(processed_dir)
 
 for file in os.listdir(DIRECTORY):
     if file != processed:
@@ -19,6 +45,7 @@ for file in os.listdir(DIRECTORY):
         # join file paths
         file_dir = os.path.join(DIRECTORY, file)
 
+        #read file
         try:
             read_file = open(file_dir, "r")
         except:
@@ -26,14 +53,11 @@ for file in os.listdir(DIRECTORY):
 
         # read file
         contents = read_file.read()
-        
-        for file_type in types:
-            if file_type.lower() in contents.lower():
-                processed_dir = os.path.join(DIRECTORY, processed)
-                if not os.path.exists(processed_dir):
-                    os.makedirs(processed_dir)
+            
+        if "invoice" in contents.lower():
+            doc_type, vendor, invoice_num = get_invoice_data(contents)
+            print(doc_type)
+            print(vendor)
+            
 
-                # Get our Document type
-                doc_type = get_doc_type(contents)
-                
-                    
+print(invoice_field)
